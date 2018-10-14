5ac9111bfe770f16195117173f5d1b5cfdd22c0c
reorr <hasbeeazam@gmail.com>
2018-10-14 20:00:13 +0000

    add eva

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Collection of themes and config that i've used. For my current config you can go
 ![Greeny-dark preview](Screenshot/greeny-dark.png)
 ### light-like-pastel
 ![light-like-pastel preview](Screenshot/light-like-pastel.png)
+### eva
+![eva preview](Screenshot/eva.png)
---
+++ eva/colors-tty.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+[ "${TERM:-none}" = "linux" ] && \
+    printf '%b' '\e]P0f6f8fa
+                 \e]P16A7B8B
+                 \e]P24C74AC
+                 \e]P3446FC1
+                 \e]P49177AE
+                 \e]P57789A0
+                 \e]P65D90CB
+                 \e]P7233B5B
+                 \e]P8787d85
+                 \e]P96A7B8B
+                 \e]PA4C74AC
+                 \e]PB446FC1
+                 \e]PC9177AE
+                 \e]PD7789A0
+                 \e]PE5D90CB
+                 \e]PF233B5B
+                 \ec'
